55cd2e1283f29a0490d7b47ec918ad265992888f
kjuanman <juanma@kde.org.ar>
2014-05-09 04:10:46 -0300

    Create es.php
    spanish translation

---
+++ l10n/es.php
@@ -0,0 +1,50 @@
+<?php $TRANSLATIONS = array(
+//Settings
+'Logs life time:' => 'Tiempo de vida de los logs:',
+'days'=>'días',
+'Save'=>'Guardar',
+
+// List filters
+'Activity'=>'Actividad',
+'Filters'=>'Filtros',
+'Since:'=>'Desde:',
+'To:'=>'hasta:',
+'Search:'=>'Buscar:',
+'Filetype:'=>'Tipo de archivo:',
+'Action:'=>'Acción:',
+'Protocol:'=>'Protocolo:',
+'All'=>'Todo',
+'Files'=>'Archivos',
+'Directories'=>'Directorios',
+'Write'=>'Escribir',
+'Delete'=>'Borrar',
+'Move'=>'Mover',
+'Rename'=>'Renombrar',
+'Web interface'=>'Interfaz web',
+'WebDav access'=>'Acceso WebDav',
+'CardDav access'=>'Acceso CardDav',
+'CalDav access'=>'Acceso CalDav',
+'logout'=>'cierre de sesión',
+'login'=>'inicio de sesión',
+'login attempt'=>'intento de inicio de sesión',
+'enable app'=>'activación de aplicación',
+'disable app'=>'desactivación de aplicación',
+
+'User'=>'Usuario',
+'Date'=>'Fecha',
+
+
+
+'More...'=>'Más...',
+
+// List items
+'Has created or modified'=>'Ha creado o modificado',
+'in'=>'en',
+'Has deleted'=>'Ha borrado',
+'from'=>'de',
+'Has moved'=>'Ha movido',
+'to'=>'a',
+'Has renamed'=>'Ha renombrado',
+'into'=>'a',
+'m.d.Y H:i:s'=>'d/m/Y H:i:s'
+);
